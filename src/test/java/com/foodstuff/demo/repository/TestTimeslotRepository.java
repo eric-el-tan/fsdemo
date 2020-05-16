f6766fbd5ee11c75d12e614c32c4e70865d063fb
eric-el-tan <eric.el.tan@gmail.com>
2020-05-17 01:56:46 +1200

    test

--- src/test/java/com/foodstuff/demo/repository/TestTimeslotRepository.java
+++ src/test/java/com/foodstuff/demo/repository/TestTimeslotRepository.java
@@ -50,6 +50,7 @@ public class TestTimeslotRepository {
     @Test
     public void TestCreateTimeslot_Abnormal(){
         assertEquals(0, timeslotRepository.getTimeslot(LocalDate.now()).size());
+
     }
 
     @Test
